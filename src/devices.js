ee893524af93bf210f59bb516bfd314d5a39c638
Tso4la1 <tsolajohn@gmail.com>
2023-11-09 12:16:07 +0100

    correct media query

--- src/devices.js
+++ src/devices.js
@@ -1,9 +1,11 @@
 const sizes={
+    desktop:`1440px`,
     medium:`768px`,
     small:`600px`
 };
 
 export const devices={
+    desk: `(max-width:${sizes.medium})`,
     mobile: `(max-width:${sizes.medium})`,
     gsm:`(max-width:${sizes.small})`
 }
